675926a09875a6e97f18807930f45259244fbd14
Jack <jack@endeavour-digital.com>
2020-05-20 15:05:23 +0100

    Added navbar and base css styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,43 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import ShoppingCart from "./icon"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+export default function Header() {
+  return (
+    <header>
+      <nav className="navbar">
+        <Link to="/" className="link">
+          <h1 className="title">Pick &amp; Mix Candy</h1>
         </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+        <ul className="navbar__links">
+          <li className="navbar__link-item">
+            <Link className="link" to="/covid-19">
+              Covid-19
+            </Link>
+          </li>
+          <li className="navbar__link-item">
+            <Link className="link" to="/products">
+              Products
+            </Link>
+          </li>
+          <li className="navbar__link-item">
+            <Link className="link" to="/about">
+              About
+            </Link>
+          </li>
+          <li className="navbar__link-item">
+            <Link className="link" to="/contact">
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link className="link" to="/basket">
+              <ShoppingCart />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
