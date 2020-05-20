45c39ccc98d1ab75d6d55d23e0554d7b450d0ac9
Jack <jack@endeavour-digital.com>
2020-05-20 11:16:32 +0100

    Updated readme and added Sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Pick and Mix Sweets`,
+    description: `Synoptic Project for Sweets pick and mix. The site is built with Gatsby and uses React. It is hosted on Netlify.`,
+    author: `@jackendeavour`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
