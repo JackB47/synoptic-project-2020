0265010f85cbb59c17817507bbd237d55ec4b101
Jack <jack@endeavour-digital.com>
2020-05-27 15:35:46 +0100

    trying to resolve sweet data issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,11 +6,12 @@
 
 // You can delete this file if you're not using it
 const path = require("path")
-const sweetData = path.resolve("src/static/data/sweets.json")
 
 exports.onCreatePage = ({ page, actions }) => {
   const { createPage } = actions
   const productTemplate = path.resolve(`src/pages/templates/product.js`)
+  const sweetData = path.resolve("src/static/data/sweets.json")
+  console.log(sweetData)
   sweetData &&
     sweetData.length > 0 &&
     sweetData.map(sweet => {
