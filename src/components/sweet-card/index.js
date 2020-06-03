7d6cdd9e76cf577e93759c1cf4f4e4e72d40e1cd
Jack <jack@endeavour-digital.com>
2020-06-03 09:04:03 +0100

    corrected a few issues, abstracted sweet-input-group and  built up single product prage

--- src/components/sweet-card/index.js
+++ src/components/sweet-card/index.js
@@ -3,6 +3,7 @@ import localForage from "localforage"
 import Button from "../button"
 import Input from "../input"
 import { Link } from "gatsby"
+import SweetInputGroup from "../sweet-input-group"
 
 export default function SweetCard({ currentSweet }) {
   const [sweet, setSweet] = useState({
@@ -11,7 +12,6 @@ export default function SweetCard({ currentSweet }) {
     price: "",
     imageUrl: "",
   })
-  const [quantity, setQuantity] = useState(0)
 
   useEffect(() => {
     const { id, type, pricePerGram, imageUrl } = currentSweet
@@ -23,37 +23,6 @@ export default function SweetCard({ currentSweet }) {
     })
   }, [currentSweet])
 
-  const handleQuantityChange = change => {
-    if (change === "dec" && quantity > 0) {
-      return setQuantity(quantity - 1)
-    } else if (change === "inc") {
-      return setQuantity(quantity + 1)
-    }
-
-    return null
-  }
-
-  const handleInputChange = e => {
-    setQuantity(parseInt(e.target.value))
-  }
-
-  const handleSubmit = async (e, sweet) => {
-    e.preventDefault()
-    const currentOrder = (await localForage.getItem("currentOrder")) || []
-    if (quantity > 0) {
-      if (currentOrder.find(item => item.id === currentSweet.id)) {
-        currentOrder.find(
-          item => item.id === currentSweet.id
-        ).quantity += quantity
-      } else {
-        currentOrder.push({ id: currentSweet.id, quantity })
-      }
-
-      setQuantity(0)
-      return localForage.setItem("currentOrder", currentOrder)
-    }
-  }
-
   return (
     <div className="sweet-card">
       <div>
@@ -62,44 +31,7 @@ export default function SweetCard({ currentSweet }) {
           <Link to={`/product/${sweet.id}`}>{sweet.type}</Link>
           <p>&pound;{sweet.price}/gram</p>
         </div>
-        <div className="sweet-card__input-group">
-          <div>
-            <Button
-              variant="pink"
-              isRound
-              onClick={() => handleQuantityChange("dec")}
-            >
-              &minus;
-            </Button>
-          </div>
-          <Input
-            className="sweet-card__input"
-            isCentered
-            min={0}
-            type="number"
-            defaultValue={0}
-            value={quantity}
-            onChange={handleInputChange}
-          />
-          <div>
-            <Button
-              variant="green"
-              isRound
-              onClick={() => handleQuantityChange("inc")}
-            >
-              &#43;
-            </Button>
-          </div>
-        </div>
-        <div>
-          <Button
-            disabled={quantity <= 0}
-            type="submit"
-            onClick={e => handleSubmit(e, sweet.type)}
-          >
-            Add to Basket
-          </Button>
-        </div>
+        <SweetInputGroup sweet={sweet} />
       </div>
     </div>
   )
